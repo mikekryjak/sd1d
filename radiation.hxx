2adb0049cb6c22a6c0e388214e876f241fa2649f
Mike <mikekryjak@gmail.com>
2022-05-27 11:19:32 +0200

    Fix ;

--- radiation.hxx
+++ radiation.hxx
@@ -75,11 +75,11 @@ public:
   // Yulin's neutral excited state population coefficients [Nn(H(n=x)) / Nn(H)]
   // Ratios of excited state to ground state populations.
   // NOTE THAT TEMPERATURE IS FIRST AND DENSITY SECOND OUTPUT
-  BoutReal Channel_H_2_amjuel(BoutReal T,BoutReal Ne)
-  BoutReal Channel_H_3_amjuel(BoutReal T,BoutReal Ne)
-  BoutReal Channel_H_4_amjuel(BoutReal T,BoutReal Ne)
-  BoutReal Channel_H_5_amjuel(BoutReal T,BoutReal Ne)
-  BoutReal Channel_H_6_amjuel(BoutReal T,BoutReal Ne)
+  BoutReal Channel_H_2_amjuel(BoutReal T,BoutReal Ne);
+  BoutReal Channel_H_3_amjuel(BoutReal T,BoutReal Ne);
+  BoutReal Channel_H_4_amjuel(BoutReal T,BoutReal Ne);
+  BoutReal Channel_H_5_amjuel(BoutReal T,BoutReal Ne);
+  BoutReal Channel_H_6_amjuel(BoutReal T,BoutReal Ne);
   
 private:
   
